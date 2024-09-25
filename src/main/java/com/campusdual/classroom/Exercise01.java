6f4fee86bf27c15b635d4e08f25b15f8fcc0c675
Sara <sarareyvalino23@gmail.com>
2024-09-25 10:58:10 +0200

    Update Exercise01.java

--- src/main/java/com/campusdual/classroom/Exercise01.java
+++ src/main/java/com/campusdual/classroom/Exercise01.java
@@ -4,6 +4,6 @@ public class Exercise01 {
 
     public static void main(String[] args) {
         //TODO → Write: System.out.println("Hello, World!");
-        System.out.println("Hello world!");
+        System.out.println("Hello, World!");
     }
 }
